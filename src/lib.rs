9c8e97eb2d739d19249780e5025fc4387f912a50
HellOwhatAs <xjq701229@outlook.com>
2025-01-31 18:00:42 +0800

    early stopping

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ pub mod data;
 pub mod utils;
 use num_traits::{Float, FloatConst};
 use rangemap::RangeMap;
+use std::cmp::{max, min, Ordering::*};
 use std::collections::BinaryHeap;
 use std::fmt::Debug;
 use utils::{float_cmp, ProjLine, ProjSegment};
@@ -13,12 +14,22 @@ where
 {
     let mut heap = BinaryHeap::from_iter(lines);
     let mut rmap: RangeMap<T, LT> = RangeMap::new();
+    let (mut lower, mut fill_all) = (T::infinity(), false);
+
     while let Some(l) = heap.pop() {
+        if fill_all {
+            if l.top_endpoint() < lower {
+                break;
+            }
+        } else {
+            if rmap.gaps(&(-T::PI()..T::PI())).next().is_none() {
+                fill_all = true;
+            }
+        }
         let doms = [l.dom, (l.dom.0, T::PI()), (-T::PI(), l.dom.1)];
         for &(dom_s, dom_e) in &doms[if l.dom.0 < l.dom.1 { 0..1 } else { 1..3 }] {
             let mut updates = vec![];
             for (seg_dom, seg_func) in rmap.overlapping(dom_s..dom_e) {
-                use std::cmp::{max, min, Ordering::*};
                 let dom = max(dom_s, seg_dom.start)..min(dom_e, seg_dom.end);
                 match (
                     float_cmp(l.line.at(dom.start), seg_func.at(dom.start), eps),
@@ -38,10 +49,13 @@ where
                     }
                 }
             }
-            for gap in rmap.gaps(&(dom_s..dom_e)) {
-                updates.push((gap, l.line));
+            if !fill_all {
+                for gap in rmap.gaps(&(dom_s..dom_e)) {
+                    updates.push((gap, l.line));
+                }
             }
             updates.into_iter().for_each(|(dom, func)| {
+                lower = min(lower, min(func.at(dom.start), func.at(dom.end)));
                 rmap.insert(dom, func);
             });
         }
